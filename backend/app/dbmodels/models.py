ee6c409153834b85ca76984bfb8e75fd728f899d
shiva-017 <dasi.s@northeastern.edu>
2025-05-03 20:03:00 -0500

    add default ordering to Team model

--- backend/app/dbmodels/models.py
+++ backend/app/dbmodels/models.py
@@ -6,6 +6,9 @@ class Team(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self) -> str:
         return self.name
 
